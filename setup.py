8330fe3245ca704ff62e15f3be76fc07e52a4e19
Anh Nguyen <anhlone3@gmail.com>
2026-09-11 19:45:07 +0700

    feat: add Python 3.12 support, include CPU/GPU demo notebooks, and introduce CI pipeline workflows.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Image Recognition",
         "Topic :: Software Development :: Libraries :: Python Modules",
